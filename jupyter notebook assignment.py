a2ba29d45f9d7d8447a2a92c89492fcf94adf5b3
Saba Anjum <102719108+s-a-b-a-786@users.noreply.github.com>
2022-09-23 22:32:45 +0530

    Add files via upload

---
+++ jupyter notebook assignment.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# ## Saba Anjum
+# Student
+
+# *I am interested in data science because i find it very interesting and want to contribute to the society by providing affordable and quality healthcare in future.*
+
+# ### The following code should print "Hello World!"
+
+# In[1]:
+
+
+print("Hello World!")
+
+
+# ----------------
+
+# | NAME | MARKS | GRADE |
+# |------|-------|-------|
+# |Saba  | 94    |  A    |
+
+# 1. India
+# 2. Austrailia
+# 3. Africa
+# 4. Amarica
+# 5. Pakistan
+
+# * rose
+# * jasmine
+# * lily
+# * sun flower
